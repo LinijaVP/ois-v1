4e25d732492ad4dabf227784f53414e0a186bec7
LinijaVP <vid.persak1@gmail.com>
2021-02-27 11:20:45 +0100

    Dodajanje izracuna pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+
+    }
+
+    public static double izracun(int dolzina) {
+        int polmer = 6371000;
+        double masaZemlje = (5.972 * Math.pow(10.0 , 24.0));
+        double gravKonstanta = (6.674 * Math.pow(10.0,-11.0));
+
+        double gravPospesek = (gravKonstanta * masaZemlje) / 
+            Math.pow(polmer + dolzina, 2.0);
+
+        return gravPospesek;
     }
 }
