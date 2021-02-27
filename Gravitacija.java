e6a63c7105a51b35d46714eb59e2bef80e2f11b3
LinijaVP <vid.persak1@gmail.com>
2021-02-27 12:06:15 +0100

    Združevanje izpisa in izracuna

--- Gravitacija.java
+++ Gravitacija.java
@@ -6,4 +6,15 @@ public class Gravitacija {
     public static void izpis(int visina, double gravPospesek) {
         System.out.printf("Na nadmorski višini %d je gravitacijski pospešek %.2f", visina, gravPospesek);
     }
+
+    public static double izracun(int dolzina) {
+        int polmer = 6371000;
+        double masaZemlje = (5.972 * Math.pow(10.0 , 24.0));
+        double gravKonstanta = (6.674 * Math.pow(10.0,-11.0));
+
+        double gravPospesek = (gravKonstanta * masaZemlje) / 
+            Math.pow(polmer + dolzina, 2.0);
+
+        return gravPospesek;
+    }
 }
